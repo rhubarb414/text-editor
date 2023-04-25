0fc5bff5e31de13aa7c5931d6576a11592b82007
rhubarb414 <ctophr.dubois@gmail.com>
2023-04-25 13:57:54 -0700

    adds promptEvent.prompt() to install click listener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,15 +4,25 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   window.deferredPrompt = event;
+  console.log('beforeinstallprompt event = ', window.preferredPrompt);
+
   butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+  console.log('clicked!');
   const promptEvents = window.deferredPrompt;
+  console.log('promptEvents = ', promptEvents);
   if (!promptEvents) {
     return;
   }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
