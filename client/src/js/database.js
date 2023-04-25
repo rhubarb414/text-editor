17ec1a7b04badeeb217a9a968e206bb213e5c546
rhubarb414 <ctophr.dubois@gmail.com>
2023-04-25 11:31:20 -0700

    heroku commit

---
+++ client/src/js/database.js
@@ -0,0 +1,37 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const results = await request;
+  console.log('Put data has been saved: ', results.value);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.get(1);
+  const results = await request;
+  // results ? results.value : console.log('No data found');
+  console.log(results);
+  return results?.value;
+};
+
+initdb();
---
+++ client/src/js/install.js
@@ -0,0 +1,21 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  const promptEvents = window.deferredPrompt;
+  if (!promptEvents) {
+    return;
+  }
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
