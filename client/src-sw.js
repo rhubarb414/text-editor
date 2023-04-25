1e6d46fe08b1c3ab87d2da49e4fb0f7bac21ebe6
rhubarb414 <ctophr.dubois@gmail.com>
2023-04-25 11:35:34 -0700

    adds image to registerRoute cache

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,7 +28,8 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 registerRoute(
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  ({ request }) =>
+    ['style', 'script', 'worker', 'image'].includes(request.destination),
   new StaleWhileRevalidate({
     // Name of the cache storage.
     cacheName: 'asset-cache',
